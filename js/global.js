2d286bc080d6b45b1c84d0ae3bc275a8d48db12a
Marvin <marvinmieth@icloud.com>
2015-05-27 07:15:32 +0200

    rake_auto_push

--- js/global.js
+++ js/global.js
@@ -609,7 +609,7 @@ window.addEventListener('resize', function() {
 				}
 			}
 		}
-		var ajaxCheckInterval = setInterval(checkIntShare, 10);
+		var ajaxCheckInterval = setInterval(checkIntShare, 100);
 		function checkIntShare() {
 			if((xhrSliced.readyState == 4 && xhrSliced.status == 200) && (xhr.readyState == 4 && xhr.status == 200)) {
 				if (getSharesSliced.shares === getShares.shares) getSharesCount /= 2;
