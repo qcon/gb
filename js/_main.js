9bcc14284134b786a94b3fe88e1c7a0589d65459
Marvin <marvinmieth@icloud.com>
2015-06-03 22:52:53 +0200

    rake_auto_push

--- js/_main.js
+++ js/_main.js
@@ -23,13 +23,53 @@ var
 
 (function(ee) {
 	if(ee) {
+		// eastereggNavbar.addEventListener('click', function() {
+		// 	_('.post--list li').forEach(function(e) {
+		// 		e.addClass('eastereggNavbar');
+		// 	});
+		// });
+		augenkrebs = [
+		'#ff00ff',
+		'#00ffff',
+		'#00ff00',
+		'#ffff00',
+		'#ff0000',
+		'#0000ff',
+		'#7920FF',
+		'#FD0987',
+		'#FF3300',
+		'#FF6EC7',
+		'#00FF66',
+		'#4D4DFF',
+		'#67C8FF',
+		'#7BFB2D',
+		'#AAFF00',
+		'#FF00AA',
+		'#AA00FF',
+		'#FF7F00',
+		'#228DFF',
+		'#ff6d38',
+		'#f90000',
+		'#fd8b25',
+		'#25d7fb',
+		'#7920ff',
+		'#fe51c3',
+		'#ff9072',
+		'#ff63a2',
+		'#e3e641',
+		];
 		eastereggNavbar.addEventListener('click', function() {
-			_('.post--list li').forEach(function(e) {
-				e.addClass('eastereggNavbar');
-			});
+			setInterval(function(){
+				_("*").forEach(function(e) {
+					e.style.backgroundColor = augenkrebs[Math.floor(Math.random()*(augenkrebs.length + 1))];
+				});
+			}, 150);
+			
 		});
 	}
 })(eastereggNavbar);
+
+
 (function(sc) {
 	if(sc) {
 		showComments.addEventListener('click', function() {
