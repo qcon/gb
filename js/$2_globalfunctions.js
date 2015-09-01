d5c4fc885ec396ee04d354c6af33bc8b2403c93c
ntwcklng <ntwcklng@gmail.com>
2015-09-01 19:47:12 +0200

    async JS, different font loading rake_auto_push

--- js/$2_globalfunctions.js
+++ js/$2_globalfunctions.js
@@ -89,19 +89,19 @@
 			console.error("AJAX Error: " + err);
 		}
 	};
-	w.WebFontConfig = {
-		google: { families: [ 'Roboto::latin' ] }
-	};
-	(function() {
-	  var wf = document.createElement('script');
-	  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-	  wf.type = 'text/javascript';
-	  wf.async = 'true';
-	  var s = document.getElementsByTagName('script')[0];
-	  s.parentNode.insertBefore(wf, s);
+	// w.WebFontConfig = {
+	// 	google: { families: [ 'Roboto::latin' ] }
+	// };
+	// (function() {
+	//   var wf = document.createElement('script');
+	//   wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+	// 	'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	//   wf.type = 'text/javascript';
+	//   wf.async = 'true';
+	//   var s = document.getElementsByTagName('script')[0];
+	//   s.parentNode.insertBefore(wf, s);
 
-	})();
+	// })();
 
 	// assign Functions to window
 	w.ajax = ajax;
