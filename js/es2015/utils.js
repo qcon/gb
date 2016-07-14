ffd5bef8796774aadd7209ebead9aa2993ade2f8
ntwcklng <ntwcklng@gmail.com>
2016-07-14 16:29:13 +0200

    :boom: m6 + beading battle 1

--- js/es2015/utils.js
+++ js/es2015/utils.js
@@ -1,7 +1,7 @@
-const $sideMenu = $('.slidemenu')
-const $sideMenuToggle = $('#slidemenutoggle')
+const $sideMenu = $('#sidemenu')
 const $fullPage = $('#fullpage')
-const $navigation = $('#mainnav')
+const $toggleNav = $('#mainnav')
+const $body = $('body')
 
 const loadingScreen = (toggle) => {
   const loadingElement = $('#loading')
@@ -16,23 +16,47 @@ const SideMenu = {}
 SideMenu.close = () => {
   $sideMenu.removeClass('menuOut')
   $fullPage.off('click', SideMenu.closeFullpage)
+  $body.off('keydown', SideMenu.closeOnEscape)
   $fullPage.removeClass('opacity03')
+  // ARIA
+  $toggleNav.attr('aria-expanded', 'false')
+  $toggleNav.attr('aria-label', 'Menü')
+  $toggleNav.attr('aria-controls', 'sidemenu')
+
+  $sideMenu.attr('aria-hidden', 'true')
+  $sideMenu.attr('aria-labelledby', 'mainnav')
+
+}
+SideMenu.open = (event) => {
+  event.stopPropagation()
+  event.preventDefault()
+  if(!$sideMenu.hasClass('menuOut')) {
+    $fullPage.addClass('opacity03')
+    $sideMenu.addClass('menuOut')
+  }
+  setTimeout(() => {
+    $toggleNav.attr('aria-expanded', 'true')
+    $sideMenu.attr('aria-hidden', 'false')
+    $fullPage.on('click', SideMenu.closeFullpage)
+    $body.on('keydown', SideMenu.closeOnEscape)
+    $('#sidemenu a:first').focus()
+  }, 100)
 }
 SideMenu.closeFullpage = (event) => {
   event.preventDefault()
   SideMenu.close()
 }
+SideMenu.closeOnEscape = (event) => {
+  if(event.which === 27) {
+    event.stopPropagation()
+    event.preventDefault()
+
+    SideMenu.close()
+  }
+}
 SideMenu.initialize = () => {
   SideMenu.close()
-  $navigation.on('click', () => {
-    if(!$sideMenu.hasClass('menuOut')) {
-      $fullPage.addClass('opacity03')
-      $sideMenu.addClass('menuOut')
-    }
-    setTimeout(() => {
-      $fullPage.on('click', SideMenu.closeFullpage)
-    }, 100)
-  })
+  $toggleNav.on('click', SideMenu.open)
 }
 SideMenu.initialize()
 
---
+++ ueber-uns.md
@@ -0,0 +1,15 @@
+---
+layout: page
+randomHeader: true
+title: Über uns
+subtitle: Wer sind eigentlich die Glossbosse oder was steckt da dahinter?
+---
+##Wer sind eigentlich die Glossbosse oder was steckt da dahinter?
+
+Angefangen hat das Ganze damit, dass ein kleiner Kreis im regen virtuellen Austausch mit Gleichgesinnten auch immer mehr privat miteinander zu tun hatte. Es wurden von uns immer mehr Fahrzeuge veredelt oder gar gerettet und die Festplatten füllten sich mit Bildern. So kam Marvin auf die Idee eine Plattform, diesen Blog, als Medium für den Austausch mit Euch zu erstellen. Damit hatten wir ein Medium, unsere Leidenschaft Fahrzeuge zu polieren, zu wachsen und zu versiegeln mit euch zu teilen. Autopflege hat nun mal viele Facetten und durch die Berichte verschiedener Glossbosse kann man zwar immer den sogenannten roten Faden erkennen, aber eben auch immer die Individualität. Für uns ist Glossboss nicht der heilige Gral aber dennoch etwas, hinter dem wir voller Stolz und mit Leidenschaft stehen.
+
+Besonders stolz macht uns die Tatsache, dass wir unabhängig Produkte testen und bewerten. Das gefällt nicht jedem Hersteller, aber auch böser Mailverkehr verdirbt uns nicht die Laune und spornt uns weiter an.
+
+Der Dialog mit Euch macht uns riesigen Spaß. Wir freuen uns über eure Kommentare unter unseren Berichten und Videos und die zahlreichen Mails die uns erreichen.
+
+1000 Likes sind eine Anerkennung, die uns motiviert euch weiter mit Berichten zu versorgen, in unserem Geheimlabor mit zu entwickeln und auch mal alles mit einer gehörigen Portion Spaß zu versehen. Es ist nun mal ein tolles und sehr umfangreichen Hobby was uns allen sehr viel Spaß macht. Die Ergebnisse unseres Hobbys bewundern wir anerkennend an Ampeln, auf Ausstellungen oder im Freundeskreis mit einem zufriedenem Lächeln.
