2284736f6ad871f8a2eb3422be8b342891296707
ntwcklng <ntwcklng@gmail.com>
2016-11-26 13:49:55 +0100

    new script

--- task/new.js
+++ task/new.js
@@ -1,33 +1,33 @@
-const fs = require('fs');
-const dateFormat = require('dateformat');
-const prompt = require('prompt');
-const path = require('path');
-const chalk = require('chalk');
+const fs = require('fs')
+const dateFormat = require('dateformat')
+const prompt = require('prompt')
+const path = require('path')
+const chalk = require('chalk')
 
-const now = new Date();
-const datePrefix = dateFormat(now, 'yyyy-mm-dd');
+const now = new Date()
+const datePrefix = dateFormat(now, 'yyyy-mm-dd')
 
 function writeFile (res) {
-  const { title, subtitle, author } = res;
-  const fileNameSlug = `${res.title.replace(/\s/g, '-').toLocaleLowerCase()}.md`;
-  const currentPath = path.resolve('./');
-  const targetPath = `${currentPath}/_preview/${datePrefix}-${fileNameSlug}`;
-  const writeStream = fs.createWriteStream(targetPath);
+  const { title, subtitle, author } = res
+  const fileNameSlug = `${res.title.replace(/\s/g, '-').toLocaleLowerCase()}.md`
+  const currentPath = path.resolve('./')
+  const targetPath = `${currentPath}/_preview/${datePrefix}-${fileNameSlug}`
+  const writeStream = fs.createWriteStream(targetPath)
   const fileContent = ['---',
-  'layout: post',
-  `title: '${title}'`,
-  `subtitle: '${subtitle}'`,
-  `author: '${author}'`,
-  'header_image: URL',
-  '---'].join('\n');
+    'layout: post',
+    `title: '${title}'`,
+    `subtitle: '${subtitle}'`,
+    `author: '${author}'`,
+    'header_image: URL',
+    '---'].join('\n')
 
-  writeStream.write(fileContent);
-  writeStream.end();
+  writeStream.write(fileContent)
+  writeStream.end()
   console.log(chalk.green(`Created ${chalk.underline.bold(fileNameSlug)} in \n${chalk.underline(targetPath)}`)); //eslint-disable-line
 }
-prompt.start();
+prompt.start()
 prompt.get(['title', 'subtitle', 'author'], (err, res) => {
-  if (err) throw err;
-  writeFile(res);
-});
+  if (err) throw err
+  writeFile(res)
+})
 
